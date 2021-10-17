b658a6b13fe645e17d23ed77b0e82bffe1f82d1c
Priyaa84 <90093240+Priyaa84@users.noreply.github.com>
2021-10-17 11:14:16 -0400

    Add files via upload

---
+++ Provider.java
@@ -0,0 +1,9 @@
+package bean;
+
+public interface Provider {
+String DRIVER="com.mysql.jdbc.Driver";
+String CONNECTION_URL="jdbc:mysql://localhost:3306/testdb?useSSL=false&user=john&password=pass1234";
+String USERNAME="john";
+String PASSWORD="pass1234";
+
+}
