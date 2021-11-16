16139d0c0c3bd2a547c1cd08eeae9633246eeb56
Priyaa84 <90093240+Priyaa84@users.noreply.github.com>
2021-11-16 10:32:12 -0500

    Modified the files to redirect the page according to the user

--- LoginDao.java
+++ LoginDao.java
@@ -2,19 +2,34 @@ package bean;
 import java.sql.*;
 public class LoginDao {
 
-	public static boolean validate(LoginBean bean){
-		boolean status=false;
+	public static int validate(LoginBean bean){
+		boolean status = false;
+		boolean is_root = false;
+		int ret_val = -1;
 		try{
 			Connection con=ConnectionProvider.getCon();
 			
-			PreparedStatement ps=con.prepareStatement("select * from login where emailid=? and password=?");
+			PreparedStatement ps=con.prepareStatement("select * from users where emailid=? and password=?");
 			ps.setString(1,bean.getEmail());
 			ps.setString(2, bean.getPass());
-			
+
+			String usernameValue = (String) bean.getEmail();
+            String root = "root";
+            if (usernameValue.equals(root))
+				is_root = true;
+				
 			ResultSet rs=ps.executeQuery();
 			status=rs.next();
 			
 		}catch(Exception e){}
-		return status;
+		
+		if (status == true)
+		{
+			if (is_root == false)
+				ret_val = 1;
+			else
+				ret_val = 0; //root user
+		}
+		return ret_val;
 	}
 }
--- loginprocess.jsp
+++ loginprocess.jsp
@@ -1,20 +1,29 @@
-
 <%@page import="bean.LoginDao"%>
 <jsp:useBean id="obj" class="bean.LoginBean"/>
 
 <jsp:setProperty property="*" name="obj"/>
 
 <%
-boolean status=LoginDao.validate(obj);
-if(status){
-out.println("You r successfully logged in");
-session.setAttribute("session","TRUE");
+int status=LoginDao.validate(obj);
+if(status == 0){
+	session.setAttribute("session","TRUE");
+	%>
+	<jsp:include page="initialize.jsp"></jsp:include>
+	<%
+}
+else if (status == 1)
+{
+	session.setAttribute("session","TRUE");
+	%>
+	<jsp:include page="Transaction.jsp"></jsp:include>
+	<%
+	
 }
 else
 {
-out.print("Sorry, email or password error");
-%>
-<jsp:include page="index.jsp"></jsp:include>
-<%
+	out.print("Sorry, email or password error");
+	%>
+	<jsp:include page="index.jsp"></jsp:include>
+	<%
 }
 %>
